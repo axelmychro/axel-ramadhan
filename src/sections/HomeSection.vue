c9a97919138bcb1bc290f647c3861b8577579167
Axel <axelmychro@gmail.com>
2026-01-11 07:52:53 +0700

    refactor(section): remove mounted logics and use css only for load-in animation

--- src/sections/HomeSection.vue
+++ src/sections/HomeSection.vue
@@ -1,13 +1,6 @@
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue'
-const mounted = ref(false)
+import { ref } from 'vue'
 
-onMounted(() => {
-  mounted.value = true
-})
-onUnmounted(() => {
-  mounted.value = false
-})
 import ProfileCard from '../components/ProfileCard.vue'
 import CTAButton from '../components/CTAButton.vue'
 
@@ -20,7 +13,7 @@ const { latestProject } = useProjects(yourProfile.projects, yourProfile.links)
   <div class="flex size-full justify-evenly not-lg:flex-col">
     <div class="flex justify-center sm:items-center">
       <div class="flex flex-col sm:gap-2">
-        <TransitionGroup name="fade-stagger">
+        <Transition name="fade-stagger" appear>
           <ProfileCard
             :author="yourProfile.name"
             :bio="yourProfile.bio"
@@ -28,19 +21,21 @@ const { latestProject } = useProjects(yourProfile.projects, yourProfile.links)
             :style="{ '--i': 0 }"
             class="not-sm:border-b-0"
           />
-        </TransitionGroup>
+        </Transition>
         <TransitionGroup
           tag="div"
           class="flex justify-between not-lg:flex-col sm:gap-2"
           name="fade-stagger"
+          appear
         >
           <CTAButton
+            key="0"
             :style="{ '--i': 1 }"
             class="not-sm:border-b-0"
             profile-link="Email"
           />
-          <CTAButton :style="{ '--i': 2 }" profile-link="LinkedIn" />
-          <CTAButton :style="{ '--i': 3 }" profile-link="Resume" />
+          <CTAButton key="1" :style="{ '--i': 2 }" profile-link="LinkedIn" />
+          <CTAButton key="2" :style="{ '--i': 3 }" profile-link="Resume" />
         </TransitionGroup>
       </div>
     </div>
@@ -48,7 +43,7 @@ const { latestProject } = useProjects(yourProfile.projects, yourProfile.links)
     <div class="flex flex-col justify-center sm:items-center">
       <div class="flex flex-col items-center">
         <div class="flex flex-col sm:gap-2 lg:gap-6">
-          <TransitionGroup name="fade-stagger">
+          <Transition name="fade-stagger" appear>
             <CTAButton
               :style="{ '--i': 0 }"
               class="not-sm:border-b-0"
@@ -59,32 +54,35 @@ const { latestProject } = useProjects(yourProfile.projects, yourProfile.links)
                 <b class="font-bold">{{ latestProject.title }}</b></span
               >
             </CTAButton>
-          </TransitionGroup>
+          </Transition>
 
           <TransitionGroup
             class="flex justify-between not-sm:flex-col sm:gap-2 lg:gap-6"
             tag="div"
             name="fade-stagger"
+            appear
           >
             <CTAButton
+              key="0"
               :style="{ '--i': 1 }"
               class="not-sm:border-b-0"
               section="About"
             />
             <CTAButton
+              key="1"
               :style="{ '--i': 2 }"
               class="not-sm:border-b-0"
               section="Timeline"
             />
           </TransitionGroup>
 
-          <TransitionGroup name="fade-stagger">
+          <Transition name="fade-stagger" appear>
             <CTAButton :style="{ '--i': 3 }" section="Contact">
               <span class="text-base tracking-wide text-pretty"
                 >I'm open for collaboration, <b>let's talk</b></span
               >
             </CTAButton>
-          </TransitionGroup>
+          </Transition>
         </div>
       </div>
     </div>
@@ -94,14 +92,12 @@ const { latestProject } = useProjects(yourProfile.projects, yourProfile.links)
 <style scoped>
 .fade-stagger-enter-active {
   transition:
-    opacity 500ms ease-out,
-    scale 500ms ease-out;
+    opacity 400ms ease-out,
+    scale 400ms ease-out;
+  transition-delay: calc(var(--i) * 200ms);
 }
 .fade-stagger-enter-from {
   opacity: 0;
   scale: 97%;
-}
-.fade-stagger-enter-active {
-  transition-delay: calc(var(--i) * 200ms);
 }
 </style>
