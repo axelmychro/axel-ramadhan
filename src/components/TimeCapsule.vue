16ff0a78e0c7cb4f654385d5323b795fb4db4ea2
Axel <axelmychro@gmail.com>
2026-01-11 18:11:07 +0700

    fix(a11y): hide decorative picture from screen-readers

--- src/components/TimeCapsule.vue
+++ src/components/TimeCapsule.vue
@@ -27,26 +27,36 @@ const formattedDate = useFormattedDate(props.date, { hideDay: true })
 
 <template>
   <article
-    class="bg-default xs:border-x-2 xs:px-2 w-full max-w-xs border-y-2 py-2 first:rounded-tl-sm last:rounded-b-sm">
+    class="bg-default xs:border-x-2 xs:px-2 w-full max-w-xs border-y-2 py-2 first:rounded-tl-sm last:rounded-b-sm"
+  >
     <header class="flex justify-between px-2">
       <span class="bg h-fit">
         <h3
-          class="font-display text-lg leading-none tracking-tight text-balance">
+          class="font-display text-lg leading-none tracking-tight text-balance"
+        >
           {{ props.title }}
         </h3>
-        <time v-if="props.date" class="text-sm" :datetime="props.date">{{
+        <time
+          v-if="props.date"
+          class="text-sm"
+          :datetime="props.date"
+        >{{
           formattedDate
         }}</time>
       </span>
 
-      <figure class="aspect-square shrink-0">
+      <figure
+        aria-hidden="true"
+        class="aspect-square shrink-0"
+      >
         <img
           v-if="props.icon"
           class="size-full max-h-8 max-w-8 object-cover object-center"
           :src="props.icon"
           width="32"
           height="32"
-          loading="lazy" />
+          loading="lazy"
+        >
       </figure>
     </header>
     <section>
