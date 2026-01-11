c4fc5c74bf0d487799b24b88fd2db591bd4bb91a
Axel <axelmychro@gmail.com>
2026-01-11 08:21:00 +0700

    chore(deps): add iconify packs and reduce sets

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import App from './App.vue'
 import { routes } from './router'
 import './assets/css/main.css'
 
+import { addCollection } from '@iconify/vue'
+import gg from '@iconify-json/gg/icons.json'
+import lucide from '@iconify-json/lucide/icons.json'
+import simpleIcons from '@iconify-json/simple-icons/icons.json'
+
+addCollection(gg, lucide, simpleIcons)
+
 export const createApp = ViteSSG(App, { routes })
