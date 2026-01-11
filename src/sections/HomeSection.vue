fd8bf846f49e2c29268c9ef33c8ba7f2cdf9b498
Axel <axelmychro@gmail.com>
2026-01-11 07:57:38 +0700

    fix(section): remove unused import

--- src/sections/HomeSection.vue
+++ src/sections/HomeSection.vue
@@ -1,6 +1,4 @@
 <script setup>
-import { ref } from 'vue'
-
 import ProfileCard from '../components/ProfileCard.vue'
 import CTAButton from '../components/CTAButton.vue'
 
@@ -13,7 +11,10 @@ const { latestProject } = useProjects(yourProfile.projects, yourProfile.links)
   <div class="flex size-full justify-evenly not-lg:flex-col">
     <div class="flex justify-center sm:items-center">
       <div class="flex flex-col sm:gap-2">
-        <Transition name="fade-stagger" appear>
+        <Transition
+          name="fade-stagger"
+          appear
+        >
           <ProfileCard
             :author="yourProfile.name"
             :bio="yourProfile.bio"
@@ -34,8 +35,16 @@ const { latestProject } = useProjects(yourProfile.projects, yourProfile.links)
             class="not-sm:border-b-0"
             profile-link="Email"
           />
-          <CTAButton key="1" :style="{ '--i': 2 }" profile-link="LinkedIn" />
-          <CTAButton key="2" :style="{ '--i': 3 }" profile-link="Resume" />
+          <CTAButton
+            key="1"
+            :style="{ '--i': 2 }"
+            profile-link="LinkedIn"
+          />
+          <CTAButton
+            key="2"
+            :style="{ '--i': 3 }"
+            profile-link="Resume"
+          />
         </TransitionGroup>
       </div>
     </div>
@@ -43,7 +52,10 @@ const { latestProject } = useProjects(yourProfile.projects, yourProfile.links)
     <div class="flex flex-col justify-center sm:items-center">
       <div class="flex flex-col items-center">
         <div class="flex flex-col sm:gap-2 lg:gap-6">
-          <Transition name="fade-stagger" appear>
+          <Transition
+            name="fade-stagger"
+            appear
+          >
             <CTAButton
               :style="{ '--i': 0 }"
               class="not-sm:border-b-0"
@@ -51,8 +63,7 @@ const { latestProject } = useProjects(yourProfile.projects, yourProfile.links)
             >
               <span class="text-base tracking-wide text-pretty">
                 View latest:
-                <b class="font-bold">{{ latestProject.title }}</b></span
-              >
+                <b class="font-bold">{{ latestProject.title }}</b></span>
             </CTAButton>
           </Transition>
 
@@ -76,11 +87,15 @@ const { latestProject } = useProjects(yourProfile.projects, yourProfile.links)
             />
           </TransitionGroup>
 
-          <Transition name="fade-stagger" appear>
-            <CTAButton :style="{ '--i': 3 }" section="Contact">
-              <span class="text-base tracking-wide text-pretty"
-                >I'm open for collaboration, <b>let's talk</b></span
-              >
+          <Transition
+            name="fade-stagger"
+            appear
+          >
+            <CTAButton
+              :style="{ '--i': 3 }"
+              section="Contact"
+            >
+              <span class="text-base tracking-wide text-pretty">I'm open for collaboration, <b>let's talk</b></span>
             </CTAButton>
           </Transition>
         </div>
