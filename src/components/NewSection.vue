d0175b093d3d56fdc1bb3f018c697e3c3c0e1051
Axel <axelmychro@gmail.com>
2026-01-13 17:55:27 +0700

    fix(section): shrink observer to detect section edges

--- src/components/NewSection.vue
+++ src/components/NewSection.vue
@@ -24,7 +24,7 @@ onMounted(() => {
       }
     },
     {
-      rootMargin: '-40% 0px -40% 0px',
+      rootMargin: '-50% 0px -50% 0px',
       threshold: 0,
     },
   )
@@ -38,7 +38,7 @@ onUnmounted(() => observer?.disconnect())
 <template>
   <section
     :id="props.id"
-    class="relative z-0 min-h-[calc(100vh-4rem)] min-w-full flex-1 shrink-0 overflow-hidden bg-linear-to-b from-sky-100 via-cyan-50 to-sky-100 pt-16 not-lg:pt-8 dark:from-neutral-900 dark:via-gray-950 dark:to-neutral-900"
+    class="relative z-0 min-h-screen min-w-full flex-1 shrink-0 overflow-hidden bg-linear-to-b from-sky-100 via-cyan-50 to-sky-100 pt-16 not-lg:pt-8 dark:from-neutral-900 dark:via-gray-950 dark:to-neutral-900"
   >
     <div
       aria-hidden="true"
