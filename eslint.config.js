da01f83847db0751eb091e935f25e1864f6ee984
Axel <axelmychro@gmail.com>
2026-01-13 22:05:23 +0700

    chore(deps): add eslint import plugin to parse Vue files

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 import js from '@eslint/js'
+import importPlugin from 'eslint-plugin-import'
+import vuePlugin from 'eslint-plugin-vue'
 import globals from 'globals'
-import pluginVue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
 
 export default [
   {
@@ -16,11 +18,17 @@ export default [
 
   js.configs.recommended,
 
-  ...pluginVue.configs['flat/recommended'],
+  ...vuePlugin.configs['flat/recommended'],
 
   {
     files: ['**/*.{js,mjs,cjs,vue}'],
+
+    plugins: {
+      import: importPlugin,
+    },
+
     languageOptions: {
+      parser: vueParser,
       ecmaVersion: 'latest',
       sourceType: 'module',
       globals: {
@@ -28,12 +36,40 @@ export default [
         ...globals.node,
       },
     },
+
+    settings: {
+      'import/parsers': {
+        'vue-eslint-parser': ['.vue'],
+      },
+    },
+
     rules: {
       'vue/multi-word-component-names': 'off',
+
       'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
       'no-debugger': 'error',
       'no-unused-vars': 'error',
       'prefer-const': 'error',
+
+      'no-unused-expressions': 'error',
+      'no-constant-binary-expression': 'error',
+      'no-extra-boolean-cast': 'error',
+
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
     },
   },
 ]
