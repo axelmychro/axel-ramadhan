725b87ecbc062414a4884c87a1b7eb976d0d08ef
Axel <axelmychro@gmail.com>
2026-01-14 09:39:11 +0700

    feat: initialize contact section

--- src/sections/ContactSection.vue
+++ src/sections/ContactSection.vue
@@ -1,5 +1,61 @@
-<script setup></script>
+<script setup>
+import { Icon } from '@iconify/vue'
+
+import { profile } from '../data/profile'
+
+const myEmail = profile.links.find(link => link.label === 'Email')
+</script>
 
 <template>
-  <div />
+  <article class="size-fit max-w-xs">
+    <header class="bg-primary border-2 p-2">
+      <h2
+        class="font-display text-invert text-end text-xl leading-none tracking-tight text-balance uppercase md:text-2xl lg:text-2xl"
+      >
+        Contact
+      </h2>
+    </header>
+
+    <section>
+      <h3
+        class="bg-invert text-default p-2 text-lg leading-none font-bold underline md:text-xl"
+      >
+        <a
+          :href="myEmail?.to"
+          class="flex gap-2"
+        >
+          <Icon
+            :icon="profile.links.find(icon => icon.label === 'Email')?.icon"
+          />{{ myEmail?.to.slice(7) }}
+        </a>
+      </h3>
+      <p
+        class="bg-default border-x-2 p-2 leading-relaxed tracking-wide text-pretty md:text-lg"
+      >
+        I'm open for internships, collaborations, and interesting discussions.
+      </p>
+    </section>
+
+    <section>
+      <h3
+        class="bg-invert text-default p-2 text-center text-base leading-none md:text-lg"
+      >
+        Also available:
+      </h3>
+      <ul>
+        <li
+          v-for="link in profile.links.slice(1, 4)"
+          :key="link.to"
+          class="bg-default border-x-2 border-b-2 p-2"
+        >
+          <a
+            class="flex items-center justify-center gap-2 text-base md:text-lg"
+            :href="link.to"
+          >
+            <Icon :icon="link.icon" />{{ link.label }}
+          </a>
+        </li>
+      </ul>
+    </section>
+  </article>
 </template>
